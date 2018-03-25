9edb3e1a946e3c55e234fbb8f8232eb33509fc83
Fanca1 <37671884+Fanca1@users.noreply.github.com>
2018-03-25 18:20:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -44,5 +44,24 @@ client.on("ready", () => {
     
 });
 
+client.on('guildMemberAdd', member => {
+
+    database.Guilds.findOne({
+        "_id": member.guild.id
+    }, function(erro, documento) {
+
+        if (documento) {
+
+            if (documento.welcome) {
+
+                var bemvindo = documento.welcomemsg
+                client.guilds.get(member.guild.id).channels.get(documento.welcomechannel).sendMessage(bemvindo.replace(/{member}/g, `<@${member.id}>`).replace(/{guild}/g, `${member.guild.name}`).replace(/{name}/g, `${member.username}`));
+
+            } else {}
+
+        } else {}
+
+    })
+
    
 client.login(token)
