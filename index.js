0b5e126b13a9079d7a61cbbf0ea0e34f6416080e
Fanca1 <37671884+Fanca1@users.noreply.github.com>
2018-03-25 10:50:54 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+console.log("Conectando...")
+const Discord = require('discord.js');
+const client = new Discord.Client({
+    autoReconnect: true,
+    max_message_cache: 0
+});
+const moment = require('moment');
+moment.locale('pt-BR');
+
+const token = process.env.token;
+
+client.on('guildMemberAdd', member => {
+    client.guilds.get(member.guild.id).channels.get("418834373610307586").send(`Olá ${member}, Obrigado por entrar no Player South - Brasil Leia as regras e tenha uma boa estadia. `, {file: {attachment: canvas.toBuffer(), name: 'welcome.gif'}});
+   })
+
+client.on("ready", () => {
+
+    let string = ''
+    for (var i = 0; i < client.users.size; i++) {
+
+        let userStatus = {
+            online: 'online',
+            idle: 'ausente',
+            dnd: 'ocupado',
+            offline: 'offline'
+        }[client.users.array()[i].presence.status]
+
+        string += "     - " + client.users.array()[i].username + " ( " + userStatus + " ) ,\n";
+    }
+
+    const membrosNomes = string
+    var statusIDO = ["idle", "dnd", "online", ]
+ 
+
+    console.log(`Conectado !`)
+    setTimeout(function() {
+        console.log(`                   ---== PLAYER SOUTH ==---                 \n\nMembros: (${client.users.size}):\n\n${membrosNomes}`);
+    }, 2000)
+    client.user.setGame("pika em " + client.users.size + " Membros");
+    client.user.setStatus(statusIDO[Math.round(Math.random() * statusIDO.length - 1)]);
+    setInterval(() => {
+        client.user.setStatus(statusIDO[Math.round(Math.random() * statusIDO.length - 1)]);
+    }, 1 * 60 * 1000)
+    
+});
+
+client.login(token)
