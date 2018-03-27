a05a106024169b7bcf3f16ef21943f67f02d38ce
Fanca1 <37671884+Fanca1@users.noreply.github.com>
2018-03-27 18:48:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ client.on("ready", () => {
 
     const membrosNomes = string
     var statusIDO = ["idle", "dnd", "online", ]
-    var jogando = [`Digite -ajuda para ver meus comandos.`, `Divulgue nosso servidor, use: -convite`, `Divulgue nosso servidor e ganhe cargos especiais! ❣️ `, `Visite nosso canal no Youtube, digite: -youtube`, `Entre no servidor do nosso Parceiro, use: -athenos`, `Estamos com vagas na staff, para mais informações chame o dono do servidor no privado. `, `Para receber cargos de jogos, utilize a sala registro`. `Player South - Brasil ${client.users.size} membros. 😉 `] 
+    var jogando = [`Digite -ajuda para ver meus comandos.`, `Divulgue nosso servidor, use: -convite`, `Divulgue nosso servidor e ganhe cargos especiais! ❣️ `, `Visite nosso canal no Youtube, digite: -youtube`, `Entre no servidor do nosso Parceiro, use: -athenos`, `Estamos com vagas na staff, para mais informações chame o dono do servidor no privado. `, `Para receber cargos de jogos, utilize a sala registro`. `Player South - Brasil ${client.users.size} membros, 😉 `] 
 
     console.log(`Conectado !`)
     setTimeout(function() {
